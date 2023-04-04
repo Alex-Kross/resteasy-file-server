88d1090be2e6b35c0c5546936d06e43a8ceafef6
Alex-Kross <artemkirlanovia@gmail.com>
2023-04-04 17:59:27 +0300

    add slf4j

--- src/main/java/com/qulix/lab/controller/FileServerController.java
+++ src/main/java/com/qulix/lab/controller/FileServerController.java
@@ -1,9 +1,11 @@
 package com.qulix.lab.controller;
 
+import ch.qos.logback.classic.Logger;
 import com.qulix.lab.service.impl.FileServerImpl;
 import org.apache.commons.io.IOUtils;
 import org.jboss.resteasy.plugins.providers.multipart.InputPart;
 import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
+import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MultivaluedMap;
@@ -14,6 +16,8 @@ import java.util.Map;
 
 @Path("/file-system")
 public class FileServerController {
+    private static final Logger logger
+            = (Logger) LoggerFactory.getLogger(FileServerController.class);
     private FileServerImpl fileServerService = new FileServerImpl();
 
     @GET
@@ -23,10 +27,13 @@ public class FileServerController {
         String answer = "";
         try {
             answer = fileServerService.getFiles(path).toString();
+            logger.info("request for get all files is success");
         } catch (FileNotFoundException e) {
+            logger.error("request for get all files is fail: " + e);
             answer = e.getMessage();
         } catch (Exception e) {
             answer = e.getMessage();
+            logger.error("request for get all files is fail: " + e);
         }
         return answer;
     }
@@ -35,6 +42,7 @@ public class FileServerController {
     @Path("/create-folder")
     @Produces("application/xml")
     public String createFolder(@QueryParam("path") String path, @QueryParam("name") String name) {
+        logger.info("request for create the folder");
         return String.valueOf(fileServerService.createFolder(path, name));
     }
 
@@ -42,6 +50,7 @@ public class FileServerController {
     @Path("/delete-folder")
     @Produces("application/xml")
     public String deleteFolder(@QueryParam("path") String path, @QueryParam("name") String name) {
+        logger.info("request for delete the folder");
         return String.valueOf(fileServerService.deleteFolder(path, name));
     }
 
@@ -63,6 +72,7 @@ public class FileServerController {
     @Consumes("multipart/form-data")
     public Response uploadFile(MultipartFormDataInput input, @QueryParam("path") String path) {
         String fileName = fileServerService.uploadFile(input, path);
+        logger.info("request for upload files on server");
         return Response.status(200)
                 .entity("uploadFile is called, Uploaded file name : " + fileName).build();
     }
@@ -71,6 +81,7 @@ public class FileServerController {
     @Path("delete")
     @Produces("application/xml")
     public String deleteFile(@QueryParam("path") String path) {
+        logger.info("request for delete the file from server");
         return String.valueOf(fileServerService.deleteFile(path));
     }
 
@@ -82,6 +93,7 @@ public class FileServerController {
         String fileName = split[split.length - 1];
         Response.ResponseBuilder response = Response.ok(file);
         response.header("Content-Disposition", "attachment; filename=" + fileName);
+        logger.info("request for download the file from server");
         return response.build();
     }
 }
